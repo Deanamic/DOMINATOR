99b7120891c77caaa396ecabb282eb6e428e44da
cesc <cescmentationfolch@users.noreply.github.com>
2016-12-02 12:36:22 +0100

    Add files via upload

--- executa.py
+++ executa.py
@@ -7,18 +7,25 @@ import subprocess
 random.seed(time.time())
 
 punt = [0,0,0,0]
-n = eval(sys.argv[2])
+guany = [0,0,0,0]
+n = eval(sys.argv[5])
 
 for i in range(0,n):
-  output = subprocess.Popen(["./Game", sys.argv[1], "Dummy", "Dummy", "Dummy", "-s", str(random.randrange(10000)), \
+  output = subprocess.Popen(["./Game", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], "-s", str(random.randrange(10000)), \
     "-i", "default.cnf", "-o", "default.res", "-d"], stdout=subprocess.PIPE).communicate()[0]
   llista = eval(output)
+  maxim = 0
+  jugador = 1
   for j in range(0,4):
     punt[j] += llista[j]
+    if maxim < llista[j]:
+        jugador = j
+        maxim = llista[j]
+  guany[jugador] += 1
   sys.stdout.write("-")
   sys.stdout.flush()
 
 print ""
 for i in range(0,4):
-  print("Jugador "+str(i)+": " + str(punt[i]//n))
+  print("Jugador "+str(i)+": " + str(punt[i]//n) + " " + str(guany[i]))
 
