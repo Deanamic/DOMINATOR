7cfed982146c5d1fac514f01df5bec4d07ca40d7
Deanamic <deanzhu1@gmail.com>
2016-12-05 14:56:17 +0100

    added support for python 3

--- Game/executa_Linux.py
+++ Game/executa_Linux.py
@@ -14,9 +14,9 @@ for i in range(0,n):
   output = subprocess.Popen(["./GameLinux", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], "-s", str(random.randrange(10000)), \
     "-i", "default.cnf", "-o", "default.res", "-d"], stdout=subprocess.PIPE).communicate()[0]
   llista = eval(output)
-  
+
   used = [0,0,0,0]
-  
+
   maxim = 0
   jugador = 1
   for j in range(0,4):
@@ -26,7 +26,7 @@ for i in range(0,n):
         maxim = llista[j]
   guany[0][jugador] += 1
   used[jugador] = 1
-  
+
   maxim = 0
   jugador = 1
   for j in range(0,4):
@@ -35,7 +35,7 @@ for i in range(0,n):
         maxim = llista[j]
   guany[1][jugador] += 1
   used[jugador] = 1
-  
+
   maxim = 0
   jugador = 1
   for j in range(0,4):
@@ -44,7 +44,7 @@ for i in range(0,n):
         maxim = llista[j]
   guany[2][jugador] += 1
   used[jugador] = 1
-  
+
   maxim = 0
   jugador = 1
   for j in range(0,4):
@@ -53,11 +53,11 @@ for i in range(0,n):
         maxim = llista[j]
   guany[3][jugador] += 1
   used[jugador] = 1
-  
+
   sys.stdout.write("-")
   sys.stdout.flush()
 
-print ""
+print ("")
 
 nom = ["","","",""]
 mitja = [0.0,0.0,0.0,0.0]
@@ -67,7 +67,7 @@ for i in range(0,4):
         nom[i] += str(" ")
     for j in range(0,4):
         mitja[i] += float(j + 1)*float(guany[j][i])/float(n)
-        
+
 for i in range(0,4):
   print("Jugador "+str(i + 1) + " " + nom[i] + ": " + str(punt[i]//n) + " " + str(guany[0][i]) + " "\
       + str(guany[1][i]) + " " + str(guany[2][i]) + " " + str(guany[3][i]) + " %.2f"  % (mitja[i]))
